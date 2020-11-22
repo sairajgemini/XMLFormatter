7fa1460c96fc7ac80ea9f94af58fa9588de3e651
Saikat Gupta <saikat.gupta1975@gmail.com>
2020-11-23 00:00:50 +0530

    Initial commit
    Second commit for Linearize Active Binary data
    
    Second commit for extra modification of variables made final

--- src/main/java/com/formatter/xml/xmlformatter/utility/XMLFileUtility.java
+++ src/main/java/com/formatter/xml/xmlformatter/utility/XMLFileUtility.java
@@ -1,5 +1,8 @@
 package com.formatter.xml.xmlformatter.utility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Files;
@@ -13,20 +16,19 @@ import java.util.Scanner;
  * This Singleton Utility class will contain all utility methods of XML formatting
  */
 public final class XMLFileUtility implements Serializable {
-    protected XMLFileUtility() {
-    }
+    protected XMLFileUtility() {}
 
-    public static final String getXmlFilePath(boolean isSrcFile) {
+    public static final String getXmlFilePath(final boolean isSrcFile) {
         Scanner in = new Scanner(System.in);
         if(isSrcFile) {
-            System.out.println("Source XML filename: ");
+            System.out.print("Source XML filename: ");
         } else {
-            System.out.println("Enter destination filename: ");
+            System.out.print("Enter destination filename: ");
         }
         return in.nextLine();
     }
 
-    public static final Path writeToFile(String src, Path dest) throws IOException {
+    public static final Path writeToFile(final String src, final Path dest) throws IOException {
         try {
             // read all lines from source Path
             List<String> lines = Files.readAllLines(Paths.get(src));
@@ -45,26 +47,49 @@ public final class XMLFileUtility implements Serializable {
         return dest;
     }
 
-    public static class XMLFormatterUtility {
+    public static final class XMLFormatterUtility {
+        // This REGEX searches all xml attributes without quotes
         static final String ATTR_EXTRACTION_REGEX = "(?<==)([^\\s\"<>]+)";
         static final String ACTIVE_BINARY_START_TAG = "<ActiveBinary>";
         static final String ACTIVE_BINARY_END_TAG = "</ActiveBinary>";
         static final String REPLACEMENT = "\"$1\"";
+        static final Logger LOGGER = LoggerFactory.getLogger(XMLFormatterUtility.class);
+
+        static boolean isActiveBinaryBlock = false;
 
         private XMLFormatterUtility() {}
 
-        public static String surroundAttrWithQuotes(final String attrValue) {
-            return "\"" + attrValue + "\"";
-        }
-
-        public static String findAndFormatXmlInput(String xmlLineInput) {
+        public static final String findAndFormatXmlInput(String xmlLineInput) {
             xmlLineInput = xmlLineInput.replaceAll(ATTR_EXTRACTION_REGEX, REPLACEMENT);
 
             if(xmlLineInput.endsWith("/\">")) {
                 xmlLineInput = xmlLineInput.replaceFirst("/\">", "\"/>");
             }
+            // Linearize Active Binary data
+            if(xmlLineInput.contains(ACTIVE_BINARY_START_TAG)) {
+                isActiveBinaryBlock = true;
+            }
+
+            if(!xmlLineInput.contains(ACTIVE_BINARY_END_TAG) && isActiveBinaryBlock) {
+                return xmlLineInput;
+            } else {
+                isActiveBinaryBlock = false;
+            }
 
             return xmlLineInput.concat("\n");
+        }
+
+        public static final void formatXMLFile() throws IOException {
+            String srcFilePath = XMLFileUtility.getXmlFilePath(true);
+            String destPath = XMLFileUtility.getXmlFilePath(false);
+            Path destFile = Paths.get(destPath);
+            if(Files.exists(destFile)) {
+                Files.delete(destFile);
+                LOGGER.info("Earlier destination file {} deleted successfully.", destPath);
+            }
+
+            destFile = Files.createFile(Paths.get(destPath));
+            XMLFileUtility.writeToFile(srcFilePath, destFile);
         }
     }
 }
